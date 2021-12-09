557b9434ffd495fd794aa46ff0c2fa77e691d50d
nonchris <git@chris-ge.de>
2021-12-10 00:42:13 +0100

    Add packaging, make script console ready

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # domain-check
+A script running over all TLDs that fulfill specified requirements, check if `name.TLD` is still free.  
--- src/checker/__init__.py
+++ src/checker/__init__.py
@@ -1,5 +1,33 @@
 # extracted from: https://www.checkdomain.net/en/
-import main
+import argparse
+
+from .main import main as _main
+
+
+def main():
+    """ Entrypoint, parsing all args, dispatching main """
+    # setup argparse and register parameters
+    parser = argparse.ArgumentParser("Check which TLDs are still free for a given name")
+    parser.add_argument("name", help="The domain name to check for")
+    parser.add_argument("-p", "--price", help="Max price for domain, default: 20.000",
+                        type=float, default=20.000, required=False)
+    parser.add_argument("-l", "--maxlen", help="Max length the TLD shall have, default: 18",
+                        type=int, default=18, required=False)
+    parser.add_argument("-r", "--rate", help="Delay between whois-calls in seconds, default: 0.2s",
+                        type=float, default=0.2, required=False)
+    parser.add_argument("-f", "--file", help="File the available results are written to, default: ./'free.txt'",
+                        type=str, default="./free.txt", required=False)
+    parser.add_argument("--preorder", help="Allow domains that are only for preorder yet, default: False",
+                        required=False, action='store_true')
+
+    args = parser.parse_args()
+
+    _main(args.name,
+          price_below=args.price,
+          request_delay=args.rate,
+          file_path=args.file,
+          only_buyable=args.preorder)
+
 
 if __name__ == '__main__':
-    main.main("besser")
+    _main("nico")
