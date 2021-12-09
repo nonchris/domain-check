83bb52ed64b3c78884c6ff5a37bac1a2d5dced1d
nonchris <git@chris-ge.de>
2021-12-10 00:36:59 +0100

    Update path for log file

--- src/checker/log_setup.py
+++ src/checker/log_setup.py
@@ -1,5 +1,6 @@
 import os
 import logging
+from pathlib import Path
 
 ### @package log_setup
 #
@@ -7,14 +8,15 @@ import logging
 #
 
 # path for databases or config files
-if not os.path.exists('data/'):
-    os.mkdir('data/')
+log_path = f'{Path.home()}/.domain-check/'
+if not os.path.exists(log_path):
+    os.mkdir(log_path)
 
 # set logging format
-formatter = logging.Formatter("[{asctime}] [{levelname}] [{name}] {message}", style="{")
+formatter = logging.Formatter("[{asctime}] [{levelname}] [{module}.{funcName}] {message}", style="{")
 
 # logger for writing to file
-file_logger = logging.FileHandler('data/events.log')
+file_logger = logging.FileHandler(f'{log_path}/events.log')
 file_logger.setFormatter(formatter)
 
 # logger for console prints
@@ -22,7 +24,7 @@ console_logger = logging.StreamHandler()
 console_logger.setFormatter(formatter)
 
 # get new logger
-logger = logging.getLogger('my-bot')
+logger = logging.getLogger('domain-check')
 logger.setLevel(logging.DEBUG)
 
 # register loggers
