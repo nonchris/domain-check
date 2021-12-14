523cd3c0d65bf6fea2d78a069caba24f9d84973d
nonchris <git@chris-ge.de>
2021-12-14 02:24:34 +0100

    Update version to 1.0.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='domain-check',
-    version='1.0.0',
+    version='1.0.1',
     description='Check which TLDs are still free for a given name',
     long_description=long_description,
     long_description_content_type='text/markdown',
